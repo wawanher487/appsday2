1b5203d760cb666a6918ebd3a3df7641ec8c904b
wawan hermawan <wawanher487@gmail.com>
2025-12-11 11:07:47 +0700

    belajar custom 404 page

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,8 +2,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="id">
+      <Head /> {/* disini kita bisa setup google analitycs, bosstrap dan lain lain */}
+
       <body>
         <Main />
         <NextScript />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
+import Head from "next/head";
 
 
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>Home Page</title>
+      </Head>
       <h1>hello world!!!!</h1>
     </div>
   )
